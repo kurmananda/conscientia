a97e03c58d4ff1438edb40fc4adf095935f58b0c
Prince Gusain <princegusain2007@gmail.com>
2026-05-24 19:36:48 +0530

    events page updated

---
+++ src/app/registration/page.jsx
@@ -0,0 +1,7 @@
+export default function NotFound(){
+    return(
+        <div className="h-screen flex items-center justify-center bg-black text-white">
+            <h1 class name="text-4xl font-bold">404- Page Not Found</h1>
+        </div>
+    );
+}
